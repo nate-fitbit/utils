74ef7c6c4e17710db7eabc9277b224552c53ea4e
Nate Goldman <ngoldman@fitbit.com>
2018-03-20 11:33:03 -0700

    Add command for elapsed time since process start

--- one-liners/misc.sh
+++ one-liners/misc.sh
@@ -1,2 +1,6 @@
 # Grep processes with fancy output: process tree w parent/child relationships, column headers
 ps -ef --forest | head -n 1; ps -ef --forest | grep -i YOUR_SEARCH_STRING
+
+# Elapsed time since STIME for a process.
+# More exact than STIME values in ps command output, since STIME can equal just the year.
+ps -o etime= -p $YOUR_PID
